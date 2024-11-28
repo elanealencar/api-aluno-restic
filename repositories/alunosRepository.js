1106630a7d1d9c513ca0f44a1fcea7c14effd504
Ester Costa de Souza <alwesterc@gmail.com>
2024-11-27 22:33:09 -0300

    adicionado o findById e corrigido o update

--- repositories/alunosRepository.js
+++ repositories/alunosRepository.js
@@ -1,47 +1,53 @@
-const {v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 let alunos = [];
 
-function create({nome, email, nome_curso}) {  
-    const aluno = {
-        id: uuidv4(),
-        nome,
-        email,
-        nome_curso
-    };
-    alunos.push(aluno);
-    return aluno;
+function create({ nome, email, nome_curso }) {
+	const aluno = {
+		id: uuidv4(),
+		nome,
+		email,
+		nome_curso
+	};
+	alunos.push(aluno);
+	return aluno;
 }
 
-function update(id, {nome, email, nome_curso}) {
-    const index = alunos.findIndex(aluno => aluno.id === id);
-    if (index === -1) {
-        return null;
-    }
-    alunos[index] = {
-        nome, 
-        email, 
-        nome_curso
-    };
-    return alunos[index];
+function update(id, { nome, email, nome_curso }) {
+	const index = alunos.findIndex((aluno) => aluno.id === id);
+	if (index === -1) {
+		return null;
+	}
+	alunos[index] = {
+		id,
+		nome,
+		email,
+		nome_curso
+	};
+	return alunos[index];
 }
 
 function remove(id) {
-    const index = alunos.findIndex(aluno => aluno.id === id);
-    if (index === -1) {
-        return false;
-    }
-    alunos.splice(index, 1);
-    return true;
+	const index = alunos.findIndex((aluno) => aluno.id === id);
+	if (index === -1) {
+		return false;
+	}
+	alunos.splice(index, 1);
+	return true;
 }
 
 function findAll() {
-    return alunos;
+	return alunos;
+}
+
+function findById(id) {
+	return alunos.find((aluno) => aluno.id === id);
 }
 
 module.exports = {
-    create,
-    update,
-    remove,
-    findAll
-}
+	create,
+	update,
+	remove,
+	findAll,
+	findById
+};
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { create, update, remove, findAll } = require('./repositories/alunosRepository');
+const {
+	create,
+	update,
+	remove,
+	findAll,
+	findById
+} = require('./repositories/alunosRepository');
 
 const app = express();
 const port = 3000;
@@ -7,29 +13,38 @@ const port = 3000;
 app.use(express.json());
 
 app.post('/alunos', (req, res) => {
-    const { nome, email, nome_curso } = req.body;
-    const aluno = create({ nome, email, nome_curso });
-    res.status(201).json(aluno);
+	const { nome, email, nome_curso } = req.body;
+	const aluno = create({ nome, email, nome_curso });
+	res.status(201).json(aluno);
 });
 
 app.get('/alunos', (req, res) => {
-    const alunos = findAll();
-    res.json(alunos);
+	const alunos = findAll();
+	res.json(alunos);
+});
+
+app.get('/alunos/:id', (req, res) => {
+	const { id } = req.params;
+	const aluno = findById(id);
+	if (!aluno) {
+		return res.status(404).json({ error: 'Aluno não encontrado' });
+	}
+	res.json(aluno);
 });
 
 app.put('/alunos/:id', (req, res) => {
-    const { id } = req.params;
-    const { nome, email, nome_curso } = req.body;
-    const aluno = update(id, { nome, email, nome_curso });
-    res.json(aluno);
+	const { id } = req.params;
+	const { nome, email, nome_curso } = req.body;
+	const aluno = update(id, { nome, email, nome_curso });
+	res.json(aluno);
 });
 
 app.delete('/alunos/:id', (req, res) => {
-    const { id } = req.params;
-    remove(id);
-    res.status(204).send();
+	const { id } = req.params;
+	remove(id);
+	res.status(204).send();
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
